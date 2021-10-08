824c879396b76eb39e68e6c7f38941ca61a09f71
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2021-10-08 14:17:22 +0530

    Update streamlit_practice.py

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -153,28 +153,28 @@ else:
 # In[ ]:
 
 st.title("5. Make a Radio Button")
-from transformers import MBartForConditionalGeneration, MBart50Tokenizer
-import streamlit as st #streamlit 
+# from transformers import MBartForConditionalGeneration, MBart50Tokenizer
+# import streamlit as st #streamlit 
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)#streamlit 
-def download_model():#streamlit 
-    model_name = "facebook/mbart-large-50-many-to-many-mmt"
-    model = MBartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = MBart50Tokenizer.from_pretrained(model_name)
-    return model, tokenizer#streamlit 
+# @st.cache(allow_output_mutation=True, suppress_st_warning=True)#streamlit 
+# def download_model():#streamlit 
+#     model_name = "facebook/mbart-large-50-many-to-many-mmt"
+#     model = MBartForConditionalGeneration.from_pretrained(model_name)
+#     tokenizer = MBart50Tokenizer.from_pretrained(model_name)
+#     return model, tokenizer#streamlit 
 
-st.title('Hindi to English Translator')#streamlit 
-text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
-model, tokenizer = download_model()#streamlit 
+# st.title('Hindi to English Translator')#streamlit 
+# text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
+# model, tokenizer = download_model()#streamlit 
 
-if st.button('Translate to English'):#streamlit 
-    if text == '':#streamlit 
-        st.write('Please enter Hindi text for translation') #streamlit 
-    else: #streamlit 
-        model_name = "facebook/mbart-large-50-many-to-many-mmt"
-        tokenizer.src_lang = "hi_IN"
-        encoded_hindi_text = tokenizer(text, return_tensors="pt")
-        generated_tokens = model.generate(**encoded_hindi_text, forced_bos_token_id=tokenizer.lang_code_to_id["en_XX"])
-        out = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)#streamlit 
-        st.write('', str(out).strip('][\''))#streamlit 
-else: pass
+# if st.button('Translate to English'):#streamlit 
+#     if text == '':#streamlit 
+#         st.write('Please enter Hindi text for translation') #streamlit 
+#     else: #streamlit 
+#         model_name = "facebook/mbart-large-50-many-to-many-mmt"
+#         tokenizer.src_lang = "hi_IN"
+#         encoded_hindi_text = tokenizer(text, return_tensors="pt")
+#         generated_tokens = model.generate(**encoded_hindi_text, forced_bos_token_id=tokenizer.lang_code_to_id["en_XX"])
+#         out = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)#streamlit 
+#         st.write('', str(out).strip('][\''))#streamlit 
+# else: pass
