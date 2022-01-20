dd176edd9d2823d0df7180a9b08d34e3b863f7e2
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-01-20 13:51:04 +0530

    Update TextSummary.py

--- TextSummary.py
+++ TextSummary.py
@@ -98,5 +98,6 @@ if st.button('Get Summary'):#for streamlit
           summary = nlargest(select_length,sentence_scores,key = sentence_scores.get)
           final_summary = [word.text for word in summary]
           summary = ' '.join(final_summary)
-          st.write('', str(summary).strip('][\''))#for streamlit
+          #st.write('', str(summary).strip('][\''))#for streamlit
+          st.write(summary)#for streamlit
 else: pass
