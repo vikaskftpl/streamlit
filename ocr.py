243b9b54988770e3feb207c2f3d2e42cd0470477
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-02-07 18:29:46 +0530

    Update ocr.py
    Added to upload a file

--- ocr.py
+++ ocr.py
@@ -7,27 +7,58 @@ Original file is located at
     https://colab.research.google.com/drive/1YnDZ5ZaaYB6QD24GOX9tcIrjXIeu9Ypl
 """
 
-!pip uninstall googletrans
-!pip install googletrans==3.1.0a0
+# !pip uninstall googletrans
+# !pip install googletrans==3.1.0a0
 
-from google.colab import drive
-drive.mount('/content/drive')
-# path = '/content/drive/My Drive/Colab Notebooks/Projects/OCR_Project/english.png'
-path = '/content/drive/My Drive/Colab Notebooks/Parag_Letter.jfif'
-open(path,mode ='r')
+import streamlit as st
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+
+
+st.title('OCR_Image_to_Text')
+
+
+# from google.colab import drive
+# drive.mount('/content/drive')
+# # path = '/content/drive/My Drive/Colab Notebooks/Projects/OCR_Project/english.png'
+# path = '/content/drive/My Drive/Colab Notebooks/Parag_Letter.jfif'
+# open(path,mode ='r')
+path = st.file_uploader("Choose a file")
+if path is not None:
+    # To read file as bytes:
+    bytes_data = path.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(path.getvalue().decode("utf-8"))
+    st.write(stringio)
+    
+    
+    
+
+#      # To read file as string:
+#      string_data = stringio.read()
+#      st.write(string_data)
+
+#      # Can be used wherever a "file-like" object is accepted:
+#      dataframe = pd.read_csv(uploaded_file)
+#      st.write(dataframe)
+
+
+
+
 
 import PIL
 from PIL import ImageDraw
 im = PIL.Image.open(path)
 im
 
-!pip uninstall opencv-python-headless==4.5.5.62
+# !pip uninstall opencv-python-headless==4.5.5.62
 
-!pip install opencv-python-headless==4.5.2.52
+# !pip install opencv-python-headless==4.5.2.52
 
-!pip install easyocr
+# !pip install easyocr
 #change run type to GPU
-!pip install googletrans
+# !pip install googletrans
 from googletrans import Translator
 
 import easyocr
@@ -35,6 +66,8 @@ reader = easyocr.Reader(['en']) #IMP 'hi'
 translator = Translator()
 bounds = reader.readtext(path,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-' )
 bounds
+
+
 
 def draw_boxes(image,bounds,color= 'yellow',width =2):
   draw = ImageDraw.Draw(image)
@@ -56,8 +89,3 @@ text_comb
 # fhand = open('OCR_Output.txt','w')
 # fhand.write(text_comb) # Erass all, already writtena and write what has been passed
 # fhand.close()
-
-
-
-
-
