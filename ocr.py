7256ffbc39fa000405c77ac87fca0f2f51d58ae7
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-02-09 15:29:36 +0530

    Update ocr.py
    added

--- ocr.py
+++ ocr.py
@@ -10,24 +10,24 @@ image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
 st.title('OCR')
 
-def load_image(image_file):    
+def load_image(image_file):
+	
 	img = Image.open(image_file)
 	return img
 
 
 if image_file is not None:
-    im = PIL.Image.open(image_file)
-    reader = easyocr.Reader(['en']) #IMP 'hi'
-    translator = Translator()
-    bounds = reader.readtext(image_file,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-' )
+	im = PIL.Image.open(image_file)
+    	reader = easyocr.Reader(['en']) #IMP 'hi'
+    	translator = Translator()
+    	bounds = reader.readtext(image_file,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-' )
 
 
-    text_list = reader.readtext(image_file,add_margin = 0.55,width_ths=0.7, link_threshold=0.8,decoder='beamsearch', blocklist='=-',detail = 0 )
-# print(text_list)
+    	text_list = reader.readtext(image_file,add_margin = 0.55,width_ths=0.7, link_threshold=0.8,decoder='beamsearch', blocklist='=-',detail = 0 )
+	#print(text_list)
 
-    text_comb =' '.join(text_list) #changed into a single line
-# print(text_comb)
-    text_comb
-
+    	text_comb =' '.join(text_list) #changed into a single line
+	#print(text_comb)
+    	text_comb
 else:
-    pass
+	pass
