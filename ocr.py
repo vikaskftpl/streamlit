b6e6958087380a9e69ab3e0180c90b37f109b839
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-02-09 18:59:45 +0530

    Update ocr.py
    added

--- ocr.py
+++ ocr.py
@@ -19,11 +19,10 @@ def load_image(image_file):
 
 if image_file is not None:
 	#im = PIL.Image.open(image_file)
-	bounds = reader.readtext(image_file,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-')
+	#bounds = reader.readtext(image_file,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-')
 	text_list = reader.readtext(image_file,add_margin = 0.55,width_ths=0.7, link_threshold=0.8,decoder='beamsearch', blocklist='=-',detail = 0 )
 	#print(text_list)
-
-    	text_comb =' '.join(text_list) #changed into a single line
+	text_comb =' '.join(text_list) #changed into a single line
 	#print(text_comb)
     	text_comb
 else:
