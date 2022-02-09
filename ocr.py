d0c63c89d024778ac17bf6c9bb98f5caeecff28f
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-02-09 19:02:10 +0530

    Update ocr.py
    changed

--- ocr.py
+++ ocr.py
@@ -10,19 +10,19 @@ translator = Translator()
 
 image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
-st.title('OCR')
 
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def load_image(image_file):
 	img = Image.open(image_file,mode = 'r')
 	return img
 
-
+st.title('OCR')
 if image_file is not None:
 	#im = PIL.Image.open(image_file)
 	#bounds = reader.readtext(image_file,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-')
 	text_list = reader.readtext(image_file,add_margin = 0.55,width_ths=0.7, link_threshold=0.8,decoder='beamsearch', blocklist='=-',detail = 0 )
 	#print(text_list)
-	text_comb =' '.join(text_list) #changed into a single line
+	text_comb =' '.join(text_list) #changed into a single line Above line commented
 	#print(text_comb)
     	text_comb
 else:
