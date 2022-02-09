324ed6bdf8dfe32434a0b08fde7067fbe019686c
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-02-09 13:32:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,25 +26,23 @@ st.title('OCR_Image_to_Text')
 import os
 image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 if image_file is not None:
-    # To See details
+	# To See details
 	file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
-    st.write(file_details)
-    # To View Uploaded Image
+    	st.write(file_details)
+    	# To View Uploaded Image
 	st.image(load_image(image_file),width=250)
 def load_image(image_file):
-    
-    img = Image.open(image_file,mode = 'r')
+	img = Image.open(image_file,mode = 'r')
 	return img
 
 bounds = reader.readtext(image_file,add_margin = 0.1,width_ths=0.5, link_threshold=0.4,decoder='beamsearch', blocklist='=-' )
 
 def draw_boxes(image_file,bounds,color= 'yellow',width =2):
-    draw = ImageDraw.Draw(image_file)
-    for bound in bounds:
-        p0,p1,p2,p3=bound[0]
-        draw.line([*p0,*p1,*p2,*p3,*p0], fill = color, width = width)
-    return image_file
-
+	draw = ImageDraw.Draw(image_file)
+	for bound in bounds:
+		p0,p1,p2,p3=bound[0]
+        	draw.line([*p0,*p1,*p2,*p3,*p0], fill = color, width = width)
+    	return image_file
 
 #draw_boxes(im, bounds)
 text_list = reader.readtext(bytes_data,add_margin = 0.55,width_ths=0.7, link_threshold=0.8,decoder='beamsearch', blocklist='=-',detail = 0 )
