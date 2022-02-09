d7464800bb4b9fce0650ff182713e4ea283fef96
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-02-09 20:28:37 +0530

    Update ocr.py
    kept outside

--- ocr.py
+++ ocr.py
@@ -21,7 +21,7 @@ reader = easyocr.Reader(['en']) #IMP 'hi'
 translator = Translator()
 
 image_file_tmp = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-
+image_file =Image.open(image_file_tmp,mode = 'r')
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def load_image(image_file_tmp):
@@ -30,7 +30,7 @@ def load_image(image_file_tmp):
 
 st.title('OCR')
 if image_file_tmp is not None:
-	image_file =Image.open(image_file_tmp,mode = 'r')
+	#image_file =Image.open(image_file_tmp,mode = 'r')
 	text_list = reader.readtext(image_file,add_margin = 0.55,width_ths=0.7, link_threshold=0.8,decoder='beamsearch', blocklist='=-',detail = 0 )
 	text_comb =' '.join(text_list)
 	st.write('', str(text_comb))#for streamlit
