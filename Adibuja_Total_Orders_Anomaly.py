cb149741e2731175ff8d765c59ef20a34c9b415e
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-06-14 17:53:43 +0530

    Update Adibuja_Total_Orders_Anomaly.py

--- Adibuja_Total_Orders_Anomaly.py
+++ Adibuja_Total_Orders_Anomaly.py
@@ -4,10 +4,10 @@ import streamlit as st
 st.title('Anomaly Detection')
 
 #!pip install fbprophet
-#@st.cache(allow_output_mutation=True, suppress_st_warning=True)#for streamlit
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)#for streamlit
 
-#import os
-#os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 import matplotlib as mpl
