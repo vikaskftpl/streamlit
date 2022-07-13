6025b8bd611b0bf006638814d2f1803458ed826f
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-07-13 15:13:06 +0530

    Update Txt_To_Audio_SPEECH.py

--- Txt_To_Audio_SPEECH.py
+++ Txt_To_Audio_SPEECH.py
@@ -8,10 +8,23 @@ from playsound import playsound
 
 st.title('Listen the ENTIRE text')
 
+import requests
+from bs4 import BeautifulSoup
+r = requests.get('https://www.adibuja.com/about-us/')
+soup = BeautifulSoup(r.content, 'html.parser')
+s = soup.find('div', class_='web_page')
+content = s.find_all('p')
+def speak ():
+  for line in content:
+    print(line.text)
+print(speak() )
+
+
 #from google.colab import drive
 #drive.mount('/content/drive')
 #%cd "/content/drive/My Drive/Colab Notebooks/Projects/NLP_ALLTextProcessing/"
-text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
+#text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
+text = st.text_area("Enter Text:", value=speak(), height=None, max_chars=None, key=None)
 
 st.write("This Web App is to help listening the text")
 if st.button('Text to SPEECH'):#for streamlit
