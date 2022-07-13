b57e22de06dc0a441b887ab8c09a32c107e9c4de
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-07-13 15:22:39 +0530

    Update Txt_To_Audio_SPEECH.py

--- Txt_To_Audio_SPEECH.py
+++ Txt_To_Audio_SPEECH.py
@@ -8,28 +8,33 @@ from playsound import playsound
 
 st.title('Listen the ENTIRE text')
 
-import requests
-from bs4 import BeautifulSoup
-r = requests.get('https://www.adibuja.com/about-us/')
-soup = BeautifulSoup(r.content, 'html.parser')
-s = soup.find('div', class_='web_page')
-content = s.find_all('p')
-for line in content:
-  print(line.text)
+
 
 #from google.colab import drive
 #drive.mount('/content/drive')
 #%cd "/content/drive/My Drive/Colab Notebooks/Projects/NLP_ALLTextProcessing/"
 #text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
-text = st.text_area("Enter Text:", value=line.text, height=None, max_chars=None, key=None)
+text = st.text_area("Enter Text:", value=' ', height=None, max_chars=None, key=None)
 
 st.write("This Web App is to help listening the text")
 if st.button('Text to SPEECH'):#for streamlit
     if text == '':
         #for streamlit
         st.write('Please enter Hindi text for translation') #for streamlit
-    else:        
+    else: 
+      
+        import requests
+        from bs4 import BeautifulSoup
+        r = requests.get('https://www.adibuja.com/about-us/')
+        soup = BeautifulSoup(r.content, 'html.parser')
+        s = soup.find('div', class_='web_page')
+        content = s.find_all('p')
+        for line in content:
+          print(line.text)
+  
         #with open('text', 'r') as file:
+    
+        data = line.text
         data = text.replace('\n', '')
         
         fhand = data.replace("\ufeff", "")
