5c9ad54aa971c73ba1f3c0ff5f0155e55853ff6a
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-07-25 17:22:42 +0530

    Update Language Translator.py
    commented

--- Language Translator.py
+++ Language Translator.py
@@ -5,7 +5,7 @@
 from transformers import MBartForConditionalGeneration, MBart50Tokenizer
 import streamlit as st #for streamlit
 
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)#for streamlit
+#@st.cache(allow_output_mutation=True, suppress_st_warning=True)#for streamlit
 def download_model():
     model_name = "facebook/mbart-large-50-many-to-many-mmt"
     model = MBartForConditionalGeneration.from_pretrained(model_name)
