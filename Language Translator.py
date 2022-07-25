1014dcc5c29078c298b701f770cc558f82ddced4
vikaskftpl <74054720+vikaskftpl@users.noreply.github.com>
2022-07-25 17:45:06 +0530

    Update Language Translator.py
    Merged

--- Language Translator.py
+++ Language Translator.py
@@ -27,19 +27,19 @@ if st.button('English to Hindi Translation'):#for streamlit
         out = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
         st.write('', str(out).strip('][\''))#for streamlit
         ###
-st.title('2. English to Marathi Translation')#for streamlit
-text = st.text_area("Enter English Text:", value='', height=None, max_chars=None, key=None)
-model, tokenizer = download_model()#for streamlit
+#st.title('2. English to Marathi Translation')#for streamlit
+#text = st.text_area("Enter English Text:", value='', height=None, max_chars=None, key=None)
+#model, tokenizer = download_model()#for streamlit
 
 if st.button('Translate to Marathi'):#for streamlit
     if text == '':#for streamlit
-        st.write('English text has been translated into Marathi') #for streamlit
+        st.write('Please enter English text for translation') #for streamlit
     else: 
         model_name = "facebook/mbart-large-50-many-to-many-mmt"
         tokenizer.src_lang = "en_XX"
-        encoded_hindi_text = tokenizer(text, return_tensors="pt")
-        generated_tokens = model.generate(**encoded_hindi_text, forced_bos_token_id=tokenizer.lang_code_to_id["mr_IN"])
-        out = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        encoded_hindi_text_1 = tokenizer(text, return_tensors="pt")
+        generated_tokens_1 = model.generate(**encoded_hindi_text_1, forced_bos_token_id=tokenizer.lang_code_to_id["mr_IN"])
+        out = tokenizer.batch_decode(generated_tokens_1, skip_special_tokens=True)
         st.write('', str(out).strip('][\''))#for streamlit
-        #####
+        
 else: pass
